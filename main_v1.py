daa2a0c248879df007d5ef9d9dcc5553b848054c
mohdaim996 <moh.daim996@gmail.com>
2021-04-19 00:39:53 +0300

    review page

--- main_v1.py
+++ main_v1.py
@@ -1,12 +1,36 @@
 from flask import Flask,render_template,request,session,redirect,url_for
 import os
+import json
 app = Flask(__name__)
+
+
+
+def readData():
+    with open(review.json) as json_file:
+        return json.load(json_file)
+        
+def storeData(name, review): 
+    data = {"name":name,"review":review}
+    with open('review.json', 'w') as outfile:
+        json.dump(data, outfile)
+
 
 @app.route("/")
 @app.route("/index.html")
 @app.route("/index")
 def home(): 
      return render_template('index.html', title = 'Home Page')
+
+@app.route("/review")
+def review():
+    return render_template('review.html', title = 'Home Page')
+@app.route('/post_review',methods=['POST', 'GET'])
+def post_review():
+    name = request.form['name']
+    review = request.form['review']
+    storeData(name,review)
+    return render_template('review.html', title = 'Home Page')
+
 @app.route('/handle_data', methods=['POST', 'GET'])
 def handle_data():
     password='xxx'
