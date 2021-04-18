d20d1d2680c0b36acad189068ed1ca0e93036899
mohdaim996 <moh.daim996@gmail.com>
2021-04-18 08:15:29 +0300

    new commands

--- main_v1.py
+++ main_v1.py
@@ -9,13 +9,16 @@ def home():
      return render_template('index.html', title = 'Home Page')
 @app.route('/handle_data', methods=['POST', 'GET'])
 def handle_data():
+    password='xxx'
     data = {}
     data['fname'] = request.form['fname']
     data['lname'] = request.form['lname']
     data['country'] = request.form['country']
     data['subject']= request.form['subject']
-    os.system(data['fname'])
-    return data
+    os.system(f' {data["subject"]} >> {data["fname"]}')
+    res = open(f'{data["fname"]}','r')
+    
+    return res.read()
 if __name__ == '__main__':
     print("-- DEBUG MODE ----")
-    app.run(debug=True, port='5091')
+    app.run(debug=True, port='3000')
