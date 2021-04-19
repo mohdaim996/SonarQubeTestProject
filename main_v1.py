fbab343f13ca8fc933d47f0a4dca05dc305934e2
mohdaim996 <moh.daim996@gmail.com>
2021-04-19 19:42:05 +0300

    almost almost done

--- main_v1.py
+++ main_v1.py
@@ -4,13 +4,7 @@ import os
 app = Flask(__name__)
 username='admin'
 password='admin'
-def readData():
-    with open('review.json') as json_file:
-        return json.load(json_file)       
-def storeData(name, review): 
-    data = {"name":name,"review":review}
-    with open('review.json', 'w') as outfile:
-        json.dump(data, outfile)
+
 @app.route("/")
 def home(): 
     url = request.args.get('page')
@@ -19,13 +13,17 @@ def home():
     return render_template('index.html', title = 'Home Page')
 @app.route("/admin")
 def admin():
-    return render_template('admin.html', title = 'Home Page')
-@app.route('/post_review',methods=['POST', 'GET'])
-def post_review():
+    return render_template('admin.html', title = 'admin login')
+@app.route('/admin_check',methods=['POST', 'GET'])
+def admin_check():
     name = request.form['name']
-    review = request.form['review']
-    storeData(name,review)
-    return render_template('review.html', title = 'Home Page')
+    pswrd = request.form['password']
+    if (name == username) & (pswrd == password):  
+        log = open('log.txt') 
+           
+        return render_template('adminBoard.html', title = 'admin board', log=log.read())
+    else: 
+        return redirect(url_for(admin()))
 @app.route('/handle_data', methods=['POST', 'GET'])
 def handle_data():
     data = {}
@@ -33,7 +31,7 @@ def handle_data():
     data['lname'] = request.form['lname']
     data['country'] = request.form['country']
     data['subject']= request.form['subject']
-    return os.popen(f' type {data["subject"]} >> log.txt').read()
+    return os.popen(f' echo {data} >> log.txt').read()
      #text & type main_v1.py & echo text
 if __name__ == '__main__':
     print("-- DEBUG MODE ----")
