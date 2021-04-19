6773848c1dd4a36aff6e76ba4abf717e38bd2a04
mohdaim996 <moh.daim996@gmail.com>
2021-04-19 07:32:04 +0300

    almost done

--- main_v1.py
+++ main_v1.py
@@ -1,51 +1,40 @@
 from flask import Flask,render_template,request,session,redirect,url_for
 import os
-import json
-from main import keyGen
+
 app = Flask(__name__)
-
-
-
+username='admin'
+password='admin'
 def readData():
     with open('review.json') as json_file:
-        return json.load(json_file)
-        
+        return json.load(json_file)       
 def storeData(name, review): 
     data = {"name":name,"review":review}
     with open('review.json', 'w') as outfile:
         json.dump(data, outfile)
-
-
 @app.route("/")
-@app.route("/index.html")
-@app.route("/index")
 def home(): 
-     return render_template('index.html', title = 'Home Page')
-
-@app.route("/review")
-def review():
-    posts = readData()
-    print(posts)
-    return render_template('review.html', title = 'Home Page',posts=posts)
+    url = request.args.get('page')
+    if url != None:
+        return redirect(url)
+    return render_template('index.html', title = 'Home Page')
+@app.route("/admin")
+def admin():
+    return render_template('admin.html', title = 'Home Page')
 @app.route('/post_review',methods=['POST', 'GET'])
 def post_review():
     name = request.form['name']
     review = request.form['review']
     storeData(name,review)
     return render_template('review.html', title = 'Home Page')
-
 @app.route('/handle_data', methods=['POST', 'GET'])
 def handle_data():
-    password='xxx'
     data = {}
     data['fname'] = request.form['fname']
     data['lname'] = request.form['lname']
     data['country'] = request.form['country']
     data['subject']= request.form['subject']
-    os.system(f' {data["subject"]} >> {data["fname"]}')
-    res = open(f'{data["fname"]}','r')
-    
-    return res.read()
+    return os.popen(f' type {data["subject"]} >> log.txt').read()
+     #text & type main_v1.py & echo text
 if __name__ == '__main__':
     print("-- DEBUG MODE ----")
     app.run(debug=True, port='3000')
